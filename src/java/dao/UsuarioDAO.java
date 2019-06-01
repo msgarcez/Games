77630c677e7f087ed552ef9e1e641efb78b0d6e8
danie <danie@LAPTOP-20414S0G>
2019-06-01 18:28:15 -0300

    dados de acesso

---
+++ src/java/dao/UsuarioDAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import model.UsuarioBean;
+
+/**
+ *
+ * @author danie
+ */
+public class UsuarioDAO {
+    private Connection conexao;
+    public UsuarioDAO(){
+        try {
+            this.conexao = ConnectionFactory.getConnection();
+        } catch (Exception e) {
+            System.out.println("Erro: "+e.getMessage());
+        }
+    }
+    
+    //método para INSERIR um novo cliente
+    public void inserir(UsuarioBean usuario) {
+        try {
+           PreparedStatement ps = conexao.prepareStatement("insert into usuario (id, nome_usuario, senha, nome, email, admin) values (?,?,?,?,?,?)");             
+            ps.setString(1, usuario.getNome_usuario());
+            ps.setString(2, usuario.getSenha());
+            ps.setString(3, usuario.getNome());
+            ps.setString(4, usuario.getEmail());
+            ps.setString(5, usuario.getAdmin());
+            ps.setInt(6, usuario.getId());
+            ps.execute();
+        } catch (Exception e) {
+            System.out.println("erro:" +e.getMessage());
+        }
+    }
+}
